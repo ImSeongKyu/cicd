aa4c80d599eb7fc1ae59c845ca41ea212d556e70
ImSeongKyu <yelook@naver.com>
2023-12-14 15:15:03 +0900

    "final 커밋"

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -13,22 +13,26 @@ public class CICDCont {
   // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
   // http://localhost:9091/cicd?sw=2
+    // http://localhost:9091/cicd?sw=3
   @GetMapping("/cicd")
   public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
     
     if (sw == -1) {
-      msg = "/cide 호출됨, error 로그";
+      msg = "/cide 호출됨, error -1 로그";
       log.error(msg);
     } else if (sw == 0) {
-      msg = "/cide 호출됨, warn 로그";
+      msg = "/cide 호출됨, warn 0 로그";
       log.warn(msg);
     } else if (sw == 1) {
-      msg = "/cicd 호출됨, info 로그";
+      msg = "/cicd 호출됨, info 1 로그";
       log.info(msg);
     } else if (sw == 2) {
-      msg = "/cide workflow 테스트";
+      msg = "/cide workflow 2 테스트";
       log.info(msg);
+    } else if (sw == 3) {
+        msg = "/cide workflow 최종 테스트";
+        log.info(msg);
     }
     
     return "<h3>" + msg + "</h3>";
