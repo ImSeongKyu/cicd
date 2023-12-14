a547fab92a8ca7c2c57e83ba18a44b73dfb78190
ImSeongKyu <yelook@naver.com>
2023-12-14 11:03:36 +0900

    2차

--- src/main/java/dev/lombok/Test.java
+++ src/main/java/dev/lombok/Test.java
@@ -17,7 +17,6 @@ public class Test {
         System.out.println("세금(%): " + devVO.getPay_tax() + " %");
 
         System.out.println(devVO.toString());
-        System.out.println(devVO.toString());
     }
 
 }
